ae19ce983a21e5c6a7a318d5c50f38850fa1151c
Priyanshu Rathore <singhpriyanshu35658@gmail.com>
2026-04-15 16:09:32 +0530

    Frontend updated with Render API URL

--- health-dashboard/src/App.jsx
+++ health-dashboard/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     setLoading(true);
     try {
       // Backend (Port 3000) se data fetch karna
-      const response = await axios.get('http://localhost:3000/health');
+      const response = await axios.get('https://health-checker-api-1zpi.onrender.com');
       setData(response.data);
     } catch (error) {
       console.error("Backend connection failed", error);
